51a9af34f882b47983dcecc075df98ea0775f61f
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-03-22 15:52:22 -0600

    created and added css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-
+import faker from 'faker'
+import CommentDetail from './CommentDetail';
 import './App.css';
 
 function App() {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar"/>
-        </a>
-        <div className='content'>
-          <a href='/' className='author'>
-            Sam
-          </a>
-          <div className='metadata'>
-            <span className='date'>Today at 6:00PM</span>
-          </div>
-          <div className='text'>Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail /> 
     </div>
   );
 }
