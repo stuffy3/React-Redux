25eb03bb3f69edf83429777ebf2757b0cf87462e
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-03-22 14:09:53 -0600

    Initialize Repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar"/>
         </a>
-      </header>
+        <div className='content'>
+          <a href='/' className='author'>
+            Sam
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 6:00PM</span>
+          </div>
+          <div className='text'>Nice blog post!</div>
+        </div>
+      </div>
     </div>
   );
 }
